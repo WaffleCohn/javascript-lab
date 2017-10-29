2d8c80d902cfcbf46de8198a048201e0367cf723
WaffleCohn <wafflecohn@gmail.com>
2017-10-29 00:27:58 -0400

    Begin Creating the Player

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var scoreBoard = document.getElementById("score"),
     startButton = document.getElementById("startButton"),
     canvas = document.getElementById("game"),
     ctx = canvas.getContext("2d");
+
+var player = {
+  hitbox: new Rectangle((canvas.width - 30) / 2, canvas.height - 60, 30, 60),
+  color: "red",
+  draw: function()
+  {
+    this.hitbox.draw(ctx, this.color);
+  }
+};

adb00d3b766bfd808b547ed29997717172242c5b
WaffleCohn <wafflecohn@gmail.com>
2017-10-29 00:24:09 -0400

    Initializing Global Variables

---
+++ main.js
@@ -0,0 +1,4 @@
+var scoreBoard = document.getElementById("score"),
+    startButton = document.getElementById("startButton"),
+    canvas = document.getElementById("game"),
+    ctx = canvas.getContext("2d");

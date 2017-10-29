44df2b7cb2806eea61beaeb7645556e2bd86016d
WaffleCohn <wafflecohn@gmail.com>
2017-10-29 00:31:48 -0400

    Drawing a Prettier Player

--- main.js
+++ main.js
@@ -4,11 +4,24 @@ var scoreBoard = document.getElementById("score"),
     ctx = canvas.getContext("2d");
 
 var player = {
+  body: [
+    new Rectangle(0, 0, 30, 10),
+    new Rectangle(0, 0, 10, 30),
+    new Rectangle(20, 0, 10, 30),
+    new Rectangle(0, 20, 30, 10),
+    new Rectangle(10, 30, 10, 10),
+    new Rectangle(0, 40, 30, 10),
+    new Rectangle(0, 50, 10, 10),
+    new Rectangle(20, 50, 10, 10)
+  ],
   hitbox: new Rectangle((canvas.width - 30) / 2, canvas.height - 60, 30, 60),
   color: "red",
   draw: function()
   {
-    this.hitbox.draw(ctx, this.color);
+    for (var i = 0; i < this.body.length; i++)
+    {
+      this.body[i].draw(ctx, this.color, this.hitbox);
+    }
   }
 };
 
